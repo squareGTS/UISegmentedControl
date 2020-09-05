ae10dbd9dd91afe6c3c206f8c7b5ac27fa1835bd
squareGTS <68609996+squareGTS@users.noreply.github.com>
2020-09-05 20:51:56 +0300

    Initial Commit

---
+++ UISegmentedControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UISegmentedControl
+//
+//  Created by Maxim Bekmetov on 05.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
