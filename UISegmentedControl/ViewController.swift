7ec7670d677ae85e124370843549b2635f023d92
squareGTS <68609996+squareGTS@users.noreply.github.com>
2020-09-09 09:44:04 +0300

    UIKit
    UIKit

--- UISegmentedControl/ViewController.swift
+++ UISegmentedControl/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var switchLabel: UILabel!
     
     
     override func viewDidLoad() {
@@ -33,6 +36,8 @@ class ViewController: UIViewController {
         slider.minimumTrackTintColor = .yellow
         slider.maximumTrackTintColor = .red
         slider.tintColor = .blue
+        
+        datePicker.locale = Locale(identifier: "ru_RU")
         
     }
 
@@ -71,6 +76,7 @@ class ViewController: UIViewController {
             
             let alert = UIAlertController(title: "Wrong format!", message: "Please enter your name", preferredStyle: .alert)
             let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+
             
             alert.addAction(okAction)
             present(alert, animated: true, completion: nil) // call action alert
@@ -81,6 +87,32 @@ class ViewController: UIViewController {
             textField.text = nil
         }
         
+    }
+    @IBAction func changeDate(_ sender: UIDatePicker) {
+        
+        let dateFormater = DateFormatter()
+        
+        dateFormater.dateStyle = .full
+        dateFormater.locale = Locale.init(identifier: "ru_RU")
+        let dateValue = dateFormater.string(from: sender.date)
+        
+        label.text = dateValue
+        
+    }
+    @IBAction func switchAction(_ sender: UISwitch) {
+        
+        segmentedControl.isHidden = !segmentedControl.isHidden
+        label.isHidden = !label.isHidden
+        slider.isHidden = !slider.isHidden
+        textField.isHidden = !textField.isHidden
+        datePicker.isHidden = !datePicker.isHidden
+        doneButton.isHidden = !doneButton.isHidden
+        
+        if sender.isOn {
+            switchLabel.text = "Show all elements"
+        } else {
+            switchLabel.text = "Hide all elements"
+        }
     }
 }
 
